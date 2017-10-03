efc14e947b750646a1b9b1b3ac4e9fefa69cabff
Alec Ge <me@alec.ge>
2017-10-03 18:39:33 -0400

    refactored code and added some documentation

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 from selenium import webdriver
-# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import os
-from time import sleep
-import platform
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import re
 
-print("this is the new code")
+
 """
-if platform.system() == 'Windows':
-    
-
 binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
 
 driver = webdriver.Firefox(firefox_binary = binary)
 """
 
+# Edit this to change the driver used.
 driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
                                            desired_capabilities=DesiredCapabilities.CHROME)
 
-driver.implicitly_wait(3)
+def main():
+    show_all_elements()
 
-driver.get('https://leetcode.com/problemset/all/')
+    # Represents problem number as will be listed in file system
+    problem = 1
+
+    while True:
+        getProblem(problem)
+        writeToFile(problem)
+        driver.get('https://leetcode.com/problemset/all/')
+        driver.implicitly_wait(4)
+        problem += 1
+
+def show_all_elements():
+    """
+    Shows all leetcode problems on a single page by clicking "all" on the dropdown at the bottom
+    :return:
+    """
+    driver.get('https://leetcode.com/problemset/all/')
+    driver.implicitly_wait(3)
+    show_all_elements = driver.find_element_by_xpath(
+        '//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select')
+
+    for option in show_all_elements.find_elements_by_tag_name('option'):
+        if option.text == 'all':
+            option.click()
+            break
 
 
+def getProblem(pNumber: int):
+    """
+    Navigates the browser to the pNumber'th problem.
 
-driver.implicitly_wait(3)
-show_all_elements = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select')
-
-for option in show_all_elements.find_elements_by_tag_name('option'):
-    if option.text == 'all':
-        option.click()
-        break
-
-
-#problems = driver.find_elements_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]/div')
-
-problem_parent = driver.find_element_by_css_selector("""#question-app > div > div:nth-child(2) > div.question-list-base > div.table-responsive.question-list-table > table > tbody.reactable-data""")
-problems_raw = problem_parent.find_elements_by_tag_name('tr')
-#print(problems)
-pNumber = 10
-
-while(True):
+    :param pNumber: The nth problem (1-indexed) so far.  Premium problems are skipped but do increment pNumber.
+    :return:
+    """
     print("> Working on problem " + str(pNumber))
-
     driver.implicitly_wait(2)
-    problem_parent = driver.find_element_by_css_selector(
-        """#question-app > div > div:nth-child(2) > div.question-list-base > div.table-responsive.question-list-table > table > tbody.reactable-data""")
+
+    problem_parent = driver.find_element_by_css_selector("""#question-app > div > div:nth-child(2) > div.question-list-base 
+                                            > div.table-responsive.question-list-table > table > tbody.reactable-data""")
     problems_raw = problem_parent.find_elements_by_tag_name('tr')
 
     problem1 = problems_raw[pNumber - 1].find_elements_by_tag_name('td')[2]
@@ -56,17 +62,41 @@ while(True):
 
     print("----> Clicking on problem")
 
+    writeToFile()
+
+
+
+def writeToFile(pNumber: int):
+    """
+    Writes the problem description and solutions in html and plain text to a file under data/
+
+    File naming conventions:
+        Problem description:
+            p<pNumber>.txt
+            e.g. p001.txt, p002.txt, p100.txt
+
+        Solution in HTML:
+            s<pNumber>.txt
+            e.g. s001.txt, s002.txt, s100.txt
+
+        Solution in plain text:
+            s<pNumber>-text.txt
+            e.g. s001-text.txt, s002-text.txt, s100-text.txt
+
+    :param pNumber: The nth problem (1-indexed) so far. Premium problems are skipped but do increment pNumber.
+    :return: Nothing
+    """
+
     if "https://leetcode.com/accounts/login/" in driver.current_url:
         driver.back()
         driver.implicitly_wait(3)
-        pNumber += 1
         print("----> Premium problem, going back")
     else:
 
         print("----> Getting description text")
         descText = driver.find_element_by_xpath('//*[@id="descriptionContent"]/div[1]/div/div[2]')
 
-        p_file_name = 'data/p' + str(pNumber) + '.txt'
+        p_file_name = 'data/p' + str(pNumber).zfill(3) + '.txt'
 
         with open(p_file_name, 'w') as w:
             w.write(descText.text)
@@ -80,29 +110,26 @@ while(True):
             solution = driver.find_element_by_xpath('//*[@id="tab-view-app"]/div/div[2]/div/div[2]/div[1]/div')
             '//*[@id="descriptionContent"]/div[1]/div/div[2]'
 
-            s_file_name = 'data/s' + str(pNumber) + '.txt'
-            s_file_name_text = 'data/s' + str(pNumber) + '-text.txt'
+            s_file_name = 'data/s' + str(pNumber).zfill(3) + '.txt'
+            s_file_name_text = 'data/s' + str(pNumber).zfill(3) + '-text.txt'
 
-            #strippedSolution = solution.get_attribute('innerHTML')
-            #strippedSolution = strippedSolution.replace(u"\u2212", "- ")
+            solution_html = solution.get_attribute('innerHTML')
 
-            solution_text = solution.get_attribute('innerHTML')
+            with open(s_file_name, 'w') as solution_html_file:
+                solution_html_file.write(solution_html)
 
+            with open(s_file_name_text, 'w') as solution_text_file:
+                solution_text_file.write(solution.text)
 
-            solution_text = re.sub('<\w*\s*(\w+=\"(\w+\s*|\w+-\w+)\")*>', "", solution_text)
-            solution_text = solution_text.replace("\\", "")
-
-            with open(s_file_name, 'w') as w:
-                w.write(solution_text)
-
-            with open(s_file_name_text, 'w') as w:
-                w.write(solution.text)
         except:
+            # No solution if any exception is thrown
             print("----> No solution")
-            s_file_name = 'data/s' + str(pNumber) + '.txt'
-            with open(s_file_name, 'w') as w:
-                w.write("No solution")
 
-    driver.get('https://leetcode.com/problemset/all/')
-    driver.implicitly_wait(4)
-    pNumber += 1
+            s_file_name = 'data/s' + str(pNumber).zfill(3) + '.txt'
+            s_file_name_text = 'data/s' + str(pNumber).zfill(3) + '-text.txt'
+
+            with open(s_file_name, 'w') as solution_html_file:
+                solution_html_file.write("No solution")
+
+            with open(s_file_name_text, 'w') as solution_text_file:
+                solution_text_file.write("No solution")
